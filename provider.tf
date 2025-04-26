c00b4a8365f054c37988eb278aced5e9b150feff
Raghavarao1201 <raghavaraomachiraju@gmail.com>
2025-04-26 14:00:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.11.4"
+}
+
 provider "aws" {
     region = "us-east-1"
 }
