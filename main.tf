02047441481d76a5529eee2acd66de46318cabee
Raghavarao1201 <raghavaraomachiraju@gmail.com>
2025-04-29 17:16:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,11 +17,12 @@ module "vpc-ec2" {
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.7.0"
-  region = "us-east-1"
+  version  = "4.7.0"
 
   bucket = "terraform-tasks"
-  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
 
   versioning = {
     enabled = true
